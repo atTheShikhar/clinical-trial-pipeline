7f53a0cc248741c3c7bec8c40c5f4eff6ea6b93f
atTheShikhar <shikharsingh.original@gmail.com>
2025-07-26 17:52:17 +0530

    fix: linting issues

--- etl/models/incremental.sql
+++ etl/models/incremental.sql
@@ -1,4 +1,4 @@
--- sqlfluff: disable=L014,L025
+-- noqa: disable=all
 MODEL (
   name studies.incremental,
   kind INCREMENTAL_BY_TIME_RANGE (
@@ -6,30 +6,28 @@ MODEL (
     partition_by_time_column false
   ),
   partitioned_by (start_year, start_month)
-);
+); 
+-- noqa: enable=all
 
-SELECT 
+
+SELECT
     protocolSection.identificationModule.nctId AS nct_id,
     protocolSection.identificationModule.briefTitle AS brief_title,
-    protocolSection.conditionsModule.conditions AS conditions,
-    LIST_TRANSFORM(protocolSection.armsInterventionsModule.interventions, x -> x.name) AS intervention_names,
+    protocolSection.conditionsModule.conditions,
     protocolSection.statusModule.overallStatus AS overall_status,
     CASE 
         WHEN try_strptime(protocolSection.statusModule.startDateStruct.date, '%Y-%m-%d') IS NOT NULL 
             THEN DATE(strptime(protocolSection.statusModule.startDateStruct.date, '%Y-%m-%d'))
         WHEN try_strptime(protocolSection.statusModule.startDateStruct.date, '%Y-%m') IS NOT NULL 
             THEN DATE(strptime(protocolSection.statusModule.startDateStruct.date || '-01', '%Y-%m-%d'))
-        ELSE 
-            NULL
     END AS start_date,
     CASE 
         WHEN try_strptime(protocolSection.statusModule.primaryCompletionDateStruct.date, '%Y-%m-%d') IS NOT NULL 
             THEN DATE(strptime(protocolSection.statusModule.primaryCompletionDateStruct.date, '%Y-%m-%d'))
         WHEN try_strptime(protocolSection.statusModule.primaryCompletionDateStruct.date, '%Y-%m') IS NOT NULL 
             THEN DATE(strptime(protocolSection.statusModule.primaryCompletionDateStruct.date || '-01', '%Y-%m-%d'))
-        ELSE 
-            NULL
     END AS completion_date,
+    LIST_TRANSFORM(protocolSection.armsInterventionsModule.interventions, x -> x.name) AS intervention_names,
     YEAR(start_date) AS start_year,
     MONTH(start_date) AS start_month
 FROM 
@@ -39,5 +37,5 @@ FROM
         auto_detect=true
     )
 WHERE 
-  start_date IS NOT NULL 
-  AND start_date BETWEEN @start_date AND @end_date;
+    start_date IS NOT NULL 
+    AND start_date BETWEEN @start_date AND @end_date;
